d2f2866d6ecbee1d0908456f503b70265445ec3e
Jakub Lamczyk <lamczykjakub@gmail.com>
2019-04-23 20:13:58 +0100

    Hubble data handled. TODO: Handle Wages data Ref: https://www.pythonforengineers.com/introduction-to-pandas/

--- __init__.py
+++ __init__.py
@@ -1,7 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-# from pylab import *
-# %pylab inline
 
 data = pd.read_csv("hubble_data.csv")
 data.head()
+data.set_index("distance", inplace=True)
+data.plot()
+plt.show()
+
+# no headers .csv handling
+# headers = ["dist", "rec_vel"]
+# data_no_headers = pd.read_csv("hubble_data_no_headers.csv", names=headers)
+# data_no_headers.set_index("dist", inplace=True)
+# data_no_headers.plot()
+# plt.show()
+
