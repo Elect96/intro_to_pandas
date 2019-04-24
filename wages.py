a2d330717faeae034fa345d9c790ae52e1f61d7b
Jakub Lamczyk <lamczykjakub@gmail.com>
2019-04-24 16:07:00 +0100

    Wages data handled. Project completed. Ref: https://www.pythonforengineers.com/introduction-to-pandas/

--- wages.py
+++ wages.py
@@ -2,5 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-data = pd.read_csv("wages_hours.csv")
-print(data)
+data = pd.read_csv("wages_hours.csv", sep="\t")
+data2 = data[["AGE", "RATE"]]
+data_sorted = data2.sort_values(["AGE"])
+data_sorted.set_index("AGE", inplace=True)
+
+data_sorted.plot()
+plt.show()
